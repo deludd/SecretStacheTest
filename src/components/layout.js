7211d20f60b126dda9fc44b790f8720306570a7c
ArtemDymytrov <adimitrov2000@gmail.com>
2023-10-11 17:46:14 +0300

    Commit import

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
-import Header from './Header'
+import Header from './header'
 
 import { GlobalStyles, Container } from '../styles/LayoutSyles'
 
