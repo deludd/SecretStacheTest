639da3eef48af1f8fd279769e9222fecf9c4fa20
ArtemDymytrov <adimitrov2000@gmail.com>
2023-10-22 21:14:34 +0300

    404 styles

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,15 @@
 import * as React from 'react';
-
 import Layout from '../components/layout';
 import Seo from '../components/seo';
+import { NotFoundWrapper, NotFoundTitle, NotFoundText } from '../styles/NotFoundPageStyles';
 
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <NotFoundWrapper>
+      <NotFoundTitle>404</NotFoundTitle>
+      <NotFoundText>Ooops! Page not found...</NotFoundText>
+    </NotFoundWrapper>
   </Layout>
 );
 
---
+++ src/styles/NotFoundPageStyles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const NotFoundWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 60vh;
+  text-align: center;
+`;
+
+export const NotFoundTitle = styled.h1`
+  font-size: 4rem;
+  color: #007bff;
+  margin-bottom: 1rem;
+`;
+
+export const NotFoundText = styled.p`
+  font-size: 1.5rem;
+  color: #666;
+`;
