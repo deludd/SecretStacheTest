b59182261e98207f1dfdd4a13c75fdc25e147fcd
ArtemDymytrov <adimitrov2000@gmail.com>
2023-11-02 15:11:53 +0200

    Add delay

--- src/utils/getAllAnimeIDs.js
+++ src/utils/getAllAnimeIDs.js
@@ -1,34 +1,39 @@
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
 const getAllAnimeIDs = async (graphql, maxAnimeCount, currentFilterValue) => {
   const bigPerPage = Math.min(Math.max(6, maxAnimeCount / 10), 50);
-  const requests = [];
   const totalPagesToIterate = Math.ceil(maxAnimeCount / bigPerPage);
+  const allAnime = [];
+
   for (let page = 1; page <= totalPagesToIterate; page++) {
-    requests.push(
-      graphql(
-        `
-          query AnimePage($page: Int!, $perPage: Int!, $currentFilterValue: ANILIST_MediaSort) {
-            anilist {
-              Page(page: $page, perPage: $perPage) {
-                media(sort: [$currentFilterValue]) {
-                  id
-                  title {
-                    userPreferred
-                  }
+    const result = await graphql(
+      `
+        query AnimePage($page: Int!, $perPage: Int!, $currentFilterValue: ANILIST_MediaSort) {
+          anilist {
+            Page(page: $page, perPage: $perPage) {
+              media(sort: [$currentFilterValue]) {
+                id
+                title {
+                  userPreferred
                 }
               }
             }
           }
-        `,
-        {
-          page: page,
-          perPage: bigPerPage,
-          currentFilterValue: currentFilterValue,
-        },
-      ),
+        }
+      `,
+      {
+        page: page,
+        perPage: bigPerPage,
+        currentFilterValue: currentFilterValue,
+      },
     );
+
+    allAnime.push(...result.data.anilist.Page.media);
+
+    if (page < totalPagesToIterate) {
+      await delay(1300);
+    }
   }
-  const results = await Promise.all(requests);
-  const allAnime = results.flatMap((result) => result.data.anilist.Page.media);
 
   return allAnime.slice(0, maxAnimeCount);
 };
