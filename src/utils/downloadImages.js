1c9c1e2bba17173febcdba3ec0f396af9f590e50
ArtemDymytrov <adimitrov2000@gmail.com>
2023-11-03 23:40:37 +0200

    Fix format of images

--- src/utils/downloadImages.js
+++ src/utils/downloadImages.js
@@ -1,36 +1,35 @@
 const fs = require('fs');
 const path = require('path');
 const download = require('image-downloader');
-
 const animeData = require('../../public/animeData.json');
 
 const IMAGES_DIR = path.join(__dirname, '../../src/images/imagesFromAPI');
 
 async function downloadImage(url, filename) {
-  try {
-    const options = {
-      url: url,
-      dest: path.join(IMAGES_DIR, filename),
-    };
-    const { filename: savedFile } = await download.image(options);
-    console.log('Saved:', savedFile);
-  } catch (error) {
-    console.error('Error downloading image:', url, error.message);
-  }
+    try {
+        const fileExtension = url.split('.').pop();
+        
+        const options = {
+            url: url,
+            dest: path.join(IMAGES_DIR, `${filename}.${fileExtension}`)
+        };
+        
+        const { filename: savedFile } = await download.image(options);
+        console.log('Saved:', savedFile);
+    } catch (error) {
+        console.error('Error downloading image:', url, error.message);
+    }
 }
 
 async function main() {
-  if (!fs.existsSync(IMAGES_DIR)) {
-    fs.mkdirSync(IMAGES_DIR);
-  }
+    if (!fs.existsSync(IMAGES_DIR)) {
+        fs.mkdirSync(IMAGES_DIR);
+    }
 
-  for (const anime of animeData) {
-    const bannerFilename = `banner_${anime.id}.jpg`;
-    await downloadImage(anime.bannerImage, bannerFilename);
-
-    const coverFilename = `cover_${anime.id}.jpg`;
-    await downloadImage(anime.coverImage, coverFilename);
-  }
+    for (const anime of animeData) {
+        await downloadImage(anime.bannerImage, `banner_${anime.id}`);
+        await downloadImage(anime.coverImage, `cover_${anime.id}`);
+    }
 }
 
 main();
